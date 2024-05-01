23e7c73ee48442b8abfc3da9a0d9b1d5649f945c
Filipe Cacique <felipe.cacique@hotmail.com>
2024-05-01 09:38:33 -0300

    Refatorando os componentes

---
+++ src/telas/Cesta/componentes/Topo.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Dimensions, Image, StyleSheet } from "react-native";
+import Texto from "../../../componentes/Texto";
+import topo from "../../../../assets/topo.png";
+
+const widthTela = Dimensions.get('screen').width;
+
+export default function Topo() {
+    return <>
+        <Image source={topo} style={estilos.topo} />
+        <Texto style={estilos.titulo}>Detalhe da Cesta</Texto>
+    </>
+}
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+        // altura e largura da imagem
+        // abaixo: altura / largura * largura da tela
+        height: 578 / 768 * widthTela
+    },
+    titulo: {
+        width: "100%",
+        position: 'absolute',
+        textAlign: 'center',
+        color: "#FFF",
+        fontSize: 16,
+        lineHeight: 26,
+        fontWeight: 'bold',
+        padding: 16
+    },
+});
